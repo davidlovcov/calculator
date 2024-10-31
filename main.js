ab7a3ec9d00ef3401ce5676b8cf3c46d42fb77ea
David Lovcov <david_lovcov@MacBook-Pro-von-David.fritz.box>
2024-10-31 20:21:32 +0100

    Add calculation functions

---
+++ main.js
@@ -0,0 +1,19 @@
+const add = (firstNumber, secondNumber) => {
+    return firstNumber + secondNumber;
+};
+
+const subract = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber;
+};
+
+const multiply = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+};
+
+const divide = (firstNumber, secondNumber) => {
+    return firstNumber / secondNumber;
+};
+
+const modulo = (firstNumber, secondNumber) => {
+    return firstNumber % secondNumber;
+};
